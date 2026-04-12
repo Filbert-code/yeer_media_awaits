036764220d25bdaacf7f76b48e290444811c8dcf
filbertalex <afilbert@slingwave.com>
2026-04-11 19:00:57 -0700

    hot fix

--- media_library.py
+++ media_library.py
@@ -23,4 +23,13 @@ MEDIA_LIBRARY = [
     #     "torrent_url": "magnet:?xt=urn:btih:EXAMPLE_HASH_HERE",
     #     "image_url": "",
     # },
+    {
+        "id": "first",
+        "title": "Pirates of the Caribbean: The Curse of the Black Pearl",
+        "year": 2003,
+        "category": "movie",
+        "description": "This swash-buckling tale follows the quest of Captain Jack Sparrow, a savvy pirate, and Will Turner, a resourceful blacksmith, as they search for Elizabeth Swann. Elizabeth, the daughter of the governor and the love of Will's life, has been kidnapped by the feared Captain Barbossa. Little do they know, but the fierce and clever Barbossa has been cursed. He, along with his large crew, are under an ancient curse, doomed for eternity to neither live, nor die. That is, unless a blood sacrifice is made. —the lexster",
+        "torrent_url": "https://en.yts-official.top/torrent/Pirates%20of%20the%20Caribbean%20The%20Curse%20of%20the%20Black%20Pearl%20(2003)%20720p.BluRay.torrent",
+        "image_url": "https://en.yts-official.top/movies/poster/pirates-of-the-caribbean-the-curse-of-the-black-pearl-2003.jpg",
+    },
 ]
